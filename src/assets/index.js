e2f4e20c7c2e18e1698d9e9734f47273eb43116c
SiriAung <thirinandaraung614@gmail.conm>
2023-01-12 11:52:36 +0630

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,67 @@
+import app from "./app.jpg";
+import hero from './hero.webp';
+import logo from './logo.png';
+import menu from './menu.svg';
+import close from './close.svg';
+import instagram from './instagram.svg';
+import twitter from './twitter.svg';
+import facebook from './facebook.svg';
+import pinterest from './pinterest.svg';
+import youtube from './youtube.svg';
+
+import pay from './pay.png';
+import play from "./play.jpg";
+import people1 from './people1.png';
+import people2 from './people2.png';
+import people3 from './people3.png';
+
+import b2 from "./banner/b2.jpg";
+import b19 from "./banner/b19.jpg";
+
+import a6 from "./about/a6.jpg";
+
+import blog1 from "./blog01.jpg";
+import blog3 from "./blog03.jpg";
+import blog4 from "./blog04.webp";
+import blog5 from "./blog02.jpg";
+
+import f01 from "./f01.png";
+import f02 from "./f02.png";
+import f03 from "./f03.png";
+import f04 from "./f04.png";
+import f05 from "./f05.png";
+import f06 from "./f06.png";
+
+import f1 from "./products/p01.jpg";
+import f2 from "./products/p02.jpg";
+import f3 from "./products/p03.jpg";
+import f4 from "./products/p04.jpg";
+import f5 from "./products/p05.jpg";
+import f6 from "./products/p06.jpg";
+import f7 from "./products/p07.jpg";
+import f8 from "./products/p08.jpg";
+
+import n1 from "./products/n01.jpg";
+import n2 from "./products/n02.jpg";
+import n3 from "./products/n03.jpg";
+import n4 from "./products/n04.jpg";
+import n5 from "./products/n05.jpg";
+import n6 from "./products/n06.jpg";
+import n7 from "./products/n07.jpg";
+import n8 from "./products/n08.jpg";
+
+export {
+  app,
+  hero,
+  logo,menu,close,
+  play,
+  pay,
+  instagram, twitter, facebook, pinterest, youtube, 
+  people1,people2,people3,
+  blog1,blog3,blog4,blog5,
+  b2, b19,a6,
+  
+  f01,f02,f03,f04,f05,f06,
+  f1, f2, f3, f4, f5, f6, f7, f8, n1, n2, n3, n4, n5, n6, n7, n8,
+
+};
